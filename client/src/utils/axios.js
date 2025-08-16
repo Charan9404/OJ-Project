213a369cbcfe2b65267fd98a2ba092c1f0694b23
Charan9404 <nany940461@gmail.com>
2025-08-17 02:45:21 +0530

    Fix Vwthcreds2  in axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// src/lib/api.js (or api.ts)
-import axios from "axios";
+// src/lib/api.js (or api.ts
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
